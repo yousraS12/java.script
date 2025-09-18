481d85e8887b5057368bc8c8249e066ff22f6a7f
yousra <boujeab2020@gmail.com>
2025-09-18 09:17:32 +0100

    condition-9

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let nombre1 = 10;
-let nombre2 = 5;
-let somme = nombre1 + nombre2;
-console.log("La somme est : " + somme);
+let age = 20;
+
+if (age < 18) {
+  console.log("Vous êtes mineur.");
+} else if (age === 18) {
+  console.log("Vous venez d’atteindre la majorité !");
+} else {
+  console.log("Vous êtes majeur.");
+}
+
+
+  
+  
