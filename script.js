d8e12c5f24d6c2459b7f7b354f1f1d927524db3a
yousra <boujeab2020@gmail.com>
2025-09-18 09:43:53 +0100

    boucles10

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-let age = 20;
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) continue; // sauter le 5
+    console.log("Nombre : " + i);
+  }
 
-if (age < 18) {
-  console.log("Vous êtes mineur.");
-} else if (age === 18) {
-  console.log("Vous venez d’atteindre la majorité !");
-} else {
-  console.log("Vous êtes majeur.");
-}
 
 
   
