92b77e81f3218911d9f98adb2f96913246b94b28
yousra <boujeab2020@gmail.com>
2025-09-18 10:07:57 +0100

    fonction 7

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-for (let i = 1; i <= 10; i++) {
-    if (i === 5) continue; // sauter le 5
-    console.log("Nombre : " + i);
+function addition(a, b) {
+    return a + b;
   }
+  
+  let somme = addition(8, 5);
+  console.log("La somme est : " + somme);
 
 
 
