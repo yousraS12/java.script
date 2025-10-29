a1e8a0a17c274b3adbcf192494bab94b1ca397c9
réalisation <boujeab2020@gmail.com>
2025-10-29 09:03:28 +0000

    chapitre 11k

---
+++ chapitre 11/script.js
@@ -0,0 +1,6 @@
+let titre = document.getElementById("titre-principal");
+console.log("Texte du titre : " + titre.innerText);
+
+let bouton = document.querySelector(".btn");
+console.log("Texte du bouton : " + bouton.innerText);
+
